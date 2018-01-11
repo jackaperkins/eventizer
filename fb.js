ef8830458890f4c6eea5af80a0af6112685bfda4
Jack Perkins <jackaperkins@gmail.com>
2018-01-11 12:34:14 +0100

    use token from env.FB_TOKEN

--- fb.js
+++ fb.js
@@ -8,44 +8,46 @@ FB SYNC
 This copies whatever events are listed in the first 'page' of results from each FB page listed in the pages table
 */
 
+var token = process.env.FB_TOKEN || config.fb_token;
 
-FB.setAccessToken (config.fb_token);
+console.log('Connecting to FB with token  ' + token.substr(0,8) + '... ');
 
+FB.setAccessToken (token);
 
-  db.page.all(function(pages) {
-    async.eachSeries(pages, function (page, callback) {
+db.page.all(function(pages) {
+  async.eachSeries(pages, function (page, callback) {
 
-      console.log("\n======== Asking Zuck for " + page.fbid);
-      FB.api(page.fbid+'/events', {fields: ['description', 'cover', 'start_time', 'end_time', 'place', 'name']}, function (res) {
-        if(!res || res.error) {
-         callback(!res ? 'error occurred' : res.error)
-         return;
-        }
-
-        console.log("Found " + res.data.length + " events:");
-        async.eachSeries(res.data, function (e, callback) {
-            console.log(e.name);
-            if(page.get('id') == null) {
-              console.log(page);
-            }
-            e.page_id = page.get('id');
-            db.event.upsert(e, callback);
-        }, callback);
-      });
-
-    }, function (err) {
-      // done!
-
-      if(err) {
-        console.log("ASYNC ERR");
-        console.log(err);
-        process.exit();
-      } else {
-        // clean old events
-        db.event.clean(function () {
-          process.exit();
-        });
+    console.log("\n======== Asking Zuck for " + page.fbid);
+    FB.api(page.fbid+'/events', {fields: ['description', 'cover', 'start_time', 'end_time', 'place', 'name']}, function (res) {
+      if(!res || res.error) {
+       callback(!res ? 'error occurred' : res.error)
+       return;
       }
 
+      console.log("Found " + res.data.length + " events");
+      async.eachSeries(res.data, function (e, callback) {
+          //console.log(e.name);
+          if(page.get('id') == null) {
+            console.log(page);
+          }
+          e.page_id = page.get('id');
+          db.event.upsert(e, callback);
+      }, callback);
     });
+
+  }, function (err) {
+    // done!
+
+    if(err) {
+      console.log("ASYNC ERR");
+      console.log(err);
+      process.exit();
+    } else {
+      // clean old events
+      db.event.clean(function () {
+        process.exit();
+      });
+    }
+
   });
+});
